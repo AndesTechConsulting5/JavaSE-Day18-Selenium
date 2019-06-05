0acd68d75db10cb87a658dbc53170cb44ccac5fe
a0z9 <a0z9@rambler.ru>
2019-06-05 12:42:50 +0300

    PageObject, v1

--- src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
+++ src/test/java/org/andestech/learning/rfb19/g3/AppTest.java
@@ -4,10 +4,21 @@ package org.andestech.learning.rfb19.g3;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -23,8 +34,151 @@ import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 import static org.testng.Assert.assertTrue;
+
+class MyEvent implements WebDriverEventListener
+{
+
+
+    @Override
+    public void beforeAlertAccept(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterAlertAccept(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeAlertDismiss(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateTo(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateTo(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateBack(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateBack(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateForward(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateForward(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterNavigateRefresh(WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeFindBy(By by, WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterFindBy(By by, WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeClickOn(WebElement webElement, WebDriver webDriver) {
+        System.out.println("+++ Clicked on " + webElement.getText());
+    }
+
+    @Override
+    public void afterClickOn(WebElement webElement, WebDriver webDriver) {
+
+
+    }
+
+    @Override
+    public void beforeChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+
+    }
+
+    @Override
+    public void afterChangeValueOf(WebElement webElement, WebDriver webDriver, CharSequence[] charSequences) {
+        System.out.println("+++ Text changed:  " + charSequences[0]);
+    }
+
+    @Override
+    public void beforeScript(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterScript(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void beforeSwitchToWindow(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterSwitchToWindow(String s, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void onException(Throwable throwable, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public <X> void beforeGetScreenshotAs(OutputType<X> outputType) {
+
+    }
+
+    @Override
+    public <X> void afterGetScreenshotAs(OutputType<X> outputType, X x) {
+
+    }
+
+    @Override
+    public void beforeGetText(WebElement webElement, WebDriver webDriver) {
+
+    }
+
+    @Override
+    public void afterGetText(WebElement webElement, WebDriver webDriver, String s) {
+
+    }
+}
+
 
 public class AppTest
 {
@@ -120,8 +274,61 @@ public class AppTest
 
 
     @Test
+    public void listenerTest(){
+        wd = new ChromeDriver(chromeOptions);
+
+        EventFiringWebDriver wd2 = new EventFiringWebDriver(wd);
+        MyEvent eventsListener = new MyEvent();
+
+        wd2.register(eventsListener);
+
+
+        wait1 = new WebDriverWait(wd2, 5);
+
+        wd2.get("http://google.com");
+        WebElement textBox = wd2.findElement(By.name("q"));
+        textBox.sendKeys("mars explorer");
+        textBox.submit();
+
+
+        wd2.quit();
+    }
+
+    @Test
+    public void loggingTest()
+    {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+        cap.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+
+        // options = new ChromeOptions();
+        chromeOptions.merge(cap);
+
+        wd = new ChromeDriver(chromeOptions);
+        wait1 = new WebDriverWait(wd, 5);
+
+        wd.get("http://newsru.com");
+
+        wd.manage().logs().getAvailableLogTypes().forEach(x -> System.out.println(x));
+       // LogEntries logEntries = wd.manage().logs().get("performance");
+        LogEntries logEntries = wd.manage().logs().get("browser");
+
+        logEntries.forEach( (LogEntry le) ->
+        {
+            System.out.printf("%d %s %s\n", le.getTimestamp(), le.getMessage(), le.getLevel().getName());
+
+        });
+
+
+    }
+
+
+    @Test
     public void loginScreenTest() throws InterruptedException
     {
+
+
         wd = new ChromeDriver(chromeOptions);
 
         Wait<WebDriver> wait1 = new WebDriverWait(wd,5);
